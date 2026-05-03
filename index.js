8b56abd9b307387df27f5230a9a497c172896480
ThejanMihisara <thejanmihisara077@gmail.com>
2026-05-03 13:26:01 +0530

    bug fix✏️

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN,
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
